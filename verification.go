dde51db607621b27e6dadf2ed2b536330dbf1e08
NiteLiite <94541816+NiteLiite@users.noreply.github.com>
2022-04-15 20:40:58 -0500

    Update verification.go

--- verification.go
+++ verification.go
@@ -6,7 +6,6 @@
  
 	Note: Please configure the variables after pasting the code into your control panel Custom Commands. Then, paste {{execCC CCIDHERE anychannelID 0 .ExecData}} into your join feed message (or just paste the full code into your join message).
  
-	Copyright (c): NiteLiite, 2022
 	Repository: https://github.com/NiteLiite/YagPDB-CC-s
 
 */}}
