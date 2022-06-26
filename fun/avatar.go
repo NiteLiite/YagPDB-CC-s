ce0f52587849e759a39f271568072dbb360f3267
NiteLiite <94541816+NiteLiite@users.noreply.github.com>
2022-06-26 10:58:28 -0500

    Update avatar.go
    fixed overlooked annoying issue when invalid args are given

--- fun/avatar.go
+++ fun/avatar.go
@@ -1,10 +1,3 @@
-{{/*
-  Trigger Type: Regex
-  Trigger \A(-|<@!?204255221017214977>\s*)(server|s|g|guild)?(avatar|av|pfp)(\s+|\z)
-
-  Usage: Type "(prefix)av help" to see how to use this! Also, if your prefix is not - replace the first - in the Trigger with your prefix!
-*/}}
-
 {{$server := reFind `(?i)(server|s|g|guild)` .Cmd}}
 {{$help := false}}
 {{$break := false}}
@@ -21,6 +14,10 @@
   {{else if reFind `((?i).*#\d{4})|(\d{17,})` (joinStr " " .CmdArgs) }}
     {{print "Hmm... did you enter that correctly? Try `" .ServerPrefix "pfp help` for help!"}}
     {{$break = true}}
+  {{else}}
+    {{if $server}}
+      {{$target = .Member}}
+    {{end}}
   {{end}}
 {{else}}
   {{if $server}}
@@ -60,7 +57,7 @@
         "fields" (cslice
         (sdict "name" "User Avatar CMDs" "value" (joinStr (print "\n" .ServerPrefix) (print .ServerPrefix "av") "avatar" "pfp"))
         (sdict "name" "Server Avatar CMDs" "value" (joinStr "\n" "s" "g" "guild" "server")) 
-            )
+        	)
         "description" (print "To get a user's **server** pfp, prepend any of the \"**Server Avatar CMDs**\" to any of the \"**User Avatar CMDs**\" *(e.g.* **" .ServerPrefix "__" (index (shuffle (cslice "s" "g" "server" "guild")) 0) "__" (index (shuffle (cslice "av" "pfp" "avatar")) 0) "** *)*")
         "title" "Avatar Command Usage:"
         "footer" (sdict "text" (print "Usage: " .ServerPrefix "CMD <member:optional>")) 
